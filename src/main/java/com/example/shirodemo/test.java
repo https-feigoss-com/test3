474e3680844e7be43cc36e664e931f3d54a416dc
caopengfei11 <caopengfei11@jd.com>
2022-07-08 17:19:39 +0800

    '1'

--- src/main/java/com/example/shirodemo/test.java
+++ src/main/java/com/example/shirodemo/test.java
@@ -1,1 +1,10 @@
 sshpass -p 'xxx' ssh root@192.168.11.11
+
+
+
+
+System.out.println(interpolator.interpolate("${script:nashron:as}")
+System.out.println(interpolator.interpolate("${loaclhost:nashron:as}")
+System.out.println(interpolator.interpolate("${sYs:nashron:as}")
+System.out.println(interpolator.interpolate("${resourceBundle:nashron:as}")
+System.out.println(interpolator.interpolate("${uRl:nashron:as}")
