914f0eebcd72cd366958cacf787fa23c647af5a6
caopengfei11 <caopengfei11@jd.com>
2022-10-11 16:24:08 +0800

    '1'

--- src/main/java/com/example/shirodemo/test.java
+++ src/main/java/com/example/shirodemo/test.java
@@ -8,3 +8,28 @@ System.out.println(interpolator.interpolate("${loaclhost:nashron:as}")
 System.out.println(interpolator.interpolate("${sYs:nashron:as}")
 System.out.println(interpolator.interpolate("${resourceBundle:nashron:as}")
 System.out.println(interpolator.interpolate("${uRl:nashron:as}")
+
+ldap_znyf
+
+ldap_znyf   ldap_znyf  sqyzkfsj
+sqyzkfsj
+
+THINK_VERSION='5.0.02'
+    THINK_VERSION ='5.0.00'
+    THINK_VERSION = '5.0.0'
+    THINK_VERSION = '5.0.2'
+    THINK_VERSION = '5.0.03'
+    THINK_VERSION=  '5.0.33'
+    THINK_VERSION =  '5.0.23'
+    THINK_VERSION   ='5.0.24'
+    THINK_VERSION ='5.1.00'
+    THINK_VERSION = '5.1.0'
+    THINK_VERSION = '5.1.2'
+    THINK_VERSION= '5.1.03'
+    THINK_VERSION =  '5.1.33'
+    THINK_VERSION =  '5.1.23'
+
+    THINK_VERSION =  '6.1.23'
+    THINK_VERSION =  '5.4.23'
+    THINK_VERSION =  '5.1.43'
+    THINK_VERSION =  '5.1.34'
