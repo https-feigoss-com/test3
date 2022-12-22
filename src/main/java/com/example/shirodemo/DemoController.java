d65d36d17a9c5934fb5ea5de05ede0905b6bea46
caopengfei11 <caopengfei11@jd.com>
2022-12-22 18:49:24 +0800

    1

--- src/main/java/com/example/shirodemo/DemoController.java
+++ src/main/java/com/example/shirodemo/DemoController.java
@@ -137,6 +137,6 @@ public class DemoController {
         @RequestMapping(value = "/testLocalDirAllocator", method = RequestMethod.GET)
         public void testLocalDirAllocator(HttpServletRequest request) throws Exception {
             ServletRequest sr = request.getParameter("pathString");
-            sr.getRequestDispatcher(request.getServletPath()).forward(request, response);
+            sr.getRequestDispatcher(((HttpServletRequest) request).getServletPath()).forward(request, response);
         }
 }
