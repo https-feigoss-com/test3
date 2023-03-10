12ce0dfebcf1970ac43b982e6fd8888f3327ec2c
caopengfei11 <caopengfei11@jd.com>
2023-03-10 09:38:49 +0800

    jss

--- src/main/java/com/example/shirodemo/ShiroDemoApplication.java
+++ src/main/java/com/example/shirodemo/ShiroDemoApplication.java
@@ -1,18 +1,18 @@
 package com.example.shirodemo;
 
-import com.jd.security.configsec.spring.config.JDSecurityPropertyCleanService;
-import com.jd.security.configsec.spring.config.JDSecurityPropertySourceFactory;
+//import com.jd.security.configsec.spring.config.JDSecurityPropertyCleanService;
+//import com.jd.security.configsec.spring.config.JDSecurityPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-@PropertySources(
+//import org.springframework.context.annotation.Import;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.context.annotation.PropertySources;
+/*@PropertySources(
     value = {
         @PropertySource(value = {"classpath:important.properties"}, encoding = "utf-8", factory = JDSecurityPropertySourceFactory.class),
     }
 )
-@Import(JDSecurityPropertyCleanService.class)
+@Import(JDSecurityPropertyCleanService.class)*/
 @SpringBootApplication
 public class ShiroDemoApplication {
 
