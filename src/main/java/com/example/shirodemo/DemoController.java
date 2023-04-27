d628fbb2ab7dc7a6fdd8900ef6b5931666fc35ca
caopengfei11 <caopengfei11@jd.com>
2023-04-27 14:33:21 +0800

    1

--- src/main/java/com/example/shirodemo/DemoController.java
+++ src/main/java/com/example/shirodemo/DemoController.java
@@ -131,9 +131,9 @@ public class DemoController {
         final ClientConfig config = new ClientConfig();
         final JingdongStorageService jss = new JingdongStorageService(new Credential("accessKey", "secretKey"), config);
 
-        final String md5 = jss.contentType(name).put();
+        final String md5 = jss.bucket("dengliang.org").contentType(name).put();
     }
-    /*bucket("dengliang.org").object("index.html").entity(new File("/export/test.html")).
+    /*object("index.html").entity(new File("/export/test.html")).
     @RequestMapping(value = "/testXXE", method = RequestMethod.GET)
     public void testXXE(final HttpServletRequest request) throws Exception {
         final String code = request.getParameter("code");
