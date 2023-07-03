c926b083694e3a3f213da8bfa3fd628e80884de2
caopengfei11 <caopengfei11@jd.com>
2023-07-03 12:18:30 +0800

    1

--- src/main/java/com/example/shirodemo/DemoController.java
+++ src/main/java/com/example/shirodemo/DemoController.java
@@ -2,6 +2,7 @@ package com.example.shirodemo;
 
 //import org.apache.hadoop.util.Shell.ShellCommandExecutor;
 
+import com.jd.sec_api.SecApi;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,6 +40,9 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;*/
 //import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.jd.jss.Credential;
 import com.jd.jss.JingdongStorageService;
@@ -46,6 +50,7 @@ import com.jd.jss.client.ClientConfig;
 /*import com.jd.sec_api.*;
 import com.jd.sec_api.extra.codecs.*;*/
 
+import javax.annotation.Resource;
 //import javax.xml.parsers.DocumentBuilder;
 //import javax.xml.parsers.DocumentBuilderFactory;
 import javax.servlet.ServletRequest;
@@ -70,82 +75,89 @@ import javax.xml.transform.Source;
 //conf import javax.xml.xquery.*;
 import java.io.InputStream;
 import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.TimeZone;
+import org.ttzero.excel.reader.ExcelReader;
+
 @RestController
 public class DemoController {
+    @Resource
+    private SecApi secapi;
 
     @RequestMapping(path = "/permit/{value}")
-    public String permit(@PathVariable final String value) {
+    public String permit(@PathVariable final String value) throws FileNotFoundException, IOException {
         // String safe = SecApi.encoder().encodeForSQL(MySQLCodec.getInstance(), value);
         // String safe = SecApi.validator().isValidSafeSqlArg(value)?"true":"false";
+
+        ExcelReader reader = ExcelReader.read(new FileInputStream(new File("D:\\caopengfei11\\Downloads\\test2.xlsx")));
+
         System.out.println("success!");
         return "ok";
     }
 
     // Another Bypass
-    // @RequestMapping(path = "/permit/*")
-    public String permit() {
-        System.out.println("success!");
-        return "success";
+    @RequestMapping(path = "/permit/url")
+    public String check(String url) {
+        return url + ":" + String.valueOf(SecApi.validator().jdSsrfExternalCheck(url));
     }
 
-    /*@RequestMapping(path = "/testJndi")
-    public String testJndi(final String url) throws NamingException, RemoteException {
-        final Properties env = new Properties();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
-        env.put(Context.PROVIDER_URL, "rmi://127.0.0.1:1099");
-        final InitialContext ctx = new InitialContext(env);
-        LocateRegistry.createRegistry(1099);
-        final Reference reference = new Reference("Evil", "Evil", url);
-        new ReferenceWrapper(reference);
-        final ReferenceWrapper referenceWrapper = new ReferenceWrapper(reference);
-        ctx.bind("evil", referenceWrapper);
-        return "success";
-    }*/
     /*
-    @RequestMapping(path = "/testcmd")
-    public void testJcmd(final String dir) throws IOException, SQLException {
-        System.out.println(dir);
-        final String[] cmdList = new String[] { "cmd", "/c", "dir " + dir };
-        // String[] cmdList = new String[]{"/bin/sh", "-c", "ls",";pwd"};
-        // String[] cmdList = new String[]{"/bin/sh", "-c", "\"ls;pwd\""};
-        final Process p = Runtime.getRuntime().exec(cmdList);
-    }
-
-    @RequestMapping(value = "/XPathTest_S_12_1_0001", method = RequestMethod.GET)
-    public NodeList XPathTest_S_12_1_0001(final Document document, final String string) {
-        final XPathFactory xPathfactory = XPathFactory.newInstance();
-        final XPath xpath = xPathfactory.newXPath();
-        try {
-            final XPathExpression xp = xpath.compile("//*[@begin]" + string);
-            final NodeList timedNodes = (NodeList) xp.evaluate(document, XPathConstants.NODESET);
-            return timedNodes;
-        } catch (final XPathExpressionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    */
+     * @RequestMapping(path = "/testJndi") public String testJndi(final String url)
+     * throws NamingException, RemoteException { final Properties env = new
+     * Properties(); env.put(Context.INITIAL_CONTEXT_FACTORY,
+     * "com.sun.jndi.rmi.registry.RegistryContextFactory");
+     * env.put(Context.PROVIDER_URL, "rmi://127.0.0.1:1099"); final InitialContext
+     * ctx = new InitialContext(env); LocateRegistry.createRegistry(1099); final
+     * Reference reference = new Reference("Evil", "Evil", url); new
+     * ReferenceWrapper(reference); final ReferenceWrapper referenceWrapper = new
+     * ReferenceWrapper(reference); ctx.bind("evil", referenceWrapper); return
+     * "success"; }
+     */
+    /*
+     * @RequestMapping(path = "/testcmd") public void testJcmd(final String dir)
+     * throws IOException, SQLException { System.out.println(dir); final String[]
+     * cmdList = new String[] { "cmd", "/c", "dir " + dir }; // String[] cmdList =
+     * new String[]{"/bin/sh", "-c", "ls",";pwd"}; // String[] cmdList = new
+     * String[]{"/bin/sh", "-c", "\"ls;pwd\""}; final Process p =
+     * Runtime.getRuntime().exec(cmdList); }
+     * 
+     * @RequestMapping(value = "/XPathTest_S_12_1_0001", method = RequestMethod.GET)
+     * public NodeList XPathTest_S_12_1_0001(final Document document, final String
+     * string) { final XPathFactory xPathfactory = XPathFactory.newInstance(); final
+     * XPath xpath = xPathfactory.newXPath(); try { final XPathExpression xp =
+     * xpath.compile("//*[@begin]" + string); final NodeList timedNodes = (NodeList)
+     * xp.evaluate(document, XPathConstants.NODESET); return timedNodes; } catch
+     * (final XPathExpressionException e) { throw new RuntimeException(e); } }
+     */
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public void handle(final HttpServletRequest request) throws Exception {
         final String name = request.getParameter("name");
         final ClientConfig config = new ClientConfig();
         final JingdongStorageService jss = new JingdongStorageService(new Credential("accessKey", "secretKey"), config);
 
-        final String md5 = jss.bucket("dengliang.org").object("index.html").a().b().entity(new File("/export/test.html")).contentType(name).put();
+        // final String md5 =
+        // jss.bucket("dengliang.org").object("index.html").a().b().entity(new
+        // File("/export/test.html")).contentType(name).put();
     }
-    /*
-    @RequestMapping(value = "/testXXE", method = RequestMethod.GET)
-    public void testXXE(final HttpServletRequest request) throws Exception {
-        final String code = request.getParameter("code");
-        final String body = request.getParameter("body");
-        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        // 禁用 DOCTYPE
-        dbf.setExpandEntityReferences(false);
-        dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-        dbf.setXIncludeAware(false);
-        final DocumentBuilder db = dbf.newDocumentBuilder();
 
-        final Document doc = db.parse(new ByteArrayInputStream(body.getBytes(code)));
-        }
-    */
+    /*
+     * @RequestMapping(value = "/testXXE", method = RequestMethod.GET) public void
+     * testXXE(final HttpServletRequest request) throws Exception { final String
+     * code = request.getParameter("code"); final String body =
+     * request.getParameter("body"); final DocumentBuilderFactory dbf =
+     * DocumentBuilderFactory.newInstance(); // 禁用 DOCTYPE
+     * dbf.setExpandEntityReferences(false);
+     * dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+     * dbf.setXIncludeAware(false); final DocumentBuilder db =
+     * dbf.newDocumentBuilder();
+     * 
+     * final Document doc = db.parse(new ByteArrayInputStream(body.getBytes(code)));
+     * }
+     */
+    public static void main(String[] args) throws URISyntaxException {
+        URI url = new URI("http://img30.360buyimg.com");
+        System.out.println(SecApi.validator().jdSsrfExternalCheck(url.toString()));
+        System.out.println(1);
+    }
 }
