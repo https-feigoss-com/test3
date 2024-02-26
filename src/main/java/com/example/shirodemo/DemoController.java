6d0376960cdb863708d0295194f849c06487f310
caopengfei11 <caopengfei11@jd.com>
2024-02-26 09:35:23 +0800

    1

--- src/main/java/com/example/shirodemo/DemoController.java
+++ src/main/java/com/example/shirodemo/DemoController.java
@@ -141,7 +141,7 @@ public class DemoController {
 
     @RequestMapping(path = "/CVE-2022-0265")
     public String cve_2022_0265(String value) throws FileNotFoundException, IOException {
-        InputStream input = String.class.getResourceAsStream("/xxe.xml");
+        InputStream input = String.class.getResourceAsStream(value);
         Config cfg = Config.loadFromStream(input);
         return "ok";
     }
